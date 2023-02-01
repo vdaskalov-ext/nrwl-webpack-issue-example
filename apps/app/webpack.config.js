a8c4f1b96ba5964df0ad4aec6424e365f168da27
Vladimir Daskalov <vdaskalov@it-economics.de>
2023-02-01 16:02:47 +0100

    fix: add terser plugin

--- apps/app/webpack.config.js
+++ apps/app/webpack.config.js
@@ -1,9 +1,15 @@
 const { composePlugins, withNx } = require('@nrwl/webpack');
 const { withReact } = require('@nrwl/react');
+const TerserPlugin = require('terser-webpack-plugin');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.optimization.minimizer.push(
+    new TerserPlugin({
+      parallel: true,
+    })
+  );
   return config;
 });
